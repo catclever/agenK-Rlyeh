6d77a901c5de193a3a181ea36b0d22cc8b587226
Kael.Cai <saint.archangel.satan@gmail.com>
2026-03-23 17:57:02 +0800

    chore: remove legacy WebContainer scripts and clean up cache artifacts

--- eyes/public/core-dist/framework/BaseComponent.d.ts
+++
@@ -1,9 +0,0 @@
-import { default as React } from 'react';
-import { BaseComponentProps } from './types';
-
-export declare function BaseComponent<T = any>({ collection, id, query, children, ...canvasProps }: BaseComponentProps & {
-    children: (props: {
-        data: any;
-        actions: any;
-    }) => React.ReactNode;
-}): import("react/jsx-runtime").JSX.Element;
--- eyes/public/core-dist/framework/CanvasContainer.d.ts
+++
@@ -1,4 +0,0 @@
-import { default as React } from 'react';
-import { CanvasProps } from './types';
-
-export declare const CanvasContainer: React.FC<CanvasProps>;
